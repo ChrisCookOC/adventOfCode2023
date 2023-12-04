1aa8e4c0a3b4f47fe1c8f61a63d3d9f09bf70f14
Chris Cook <114592674+ChrisCookOC@users.noreply.github.com>
2023-12-04 12:53:29 +0000

    Day 1 part 2

--- src/main/scala/aoc/day1/Day1.scala
+++ src/main/scala/aoc/day1/Day1.scala
@@ -6,18 +6,48 @@ import scala.io.{BufferedSource, Source}
 case class Day1() {
   def createNumberFromSet(list: Seq[Int]): Int = list.mkString("").toInt
 
-  def getFirstAndLastDigitFromList(list: Seq[Int]): Seq[Int] =
-    if (list.nonEmpty) Seq(list.head, list.last) else Seq.empty
+  def getFirstAndLastDigit(str: String): Seq[Int] = {
 
-  def getDigitsFromLine(str: String): Seq[Int] = {
-    str.collect { case x if x.isDigit => x.toString.toInt}
+    val regex = "one|two|three|four|five|six|seven|eight|nine|\\d".r
+    val backwardsRegex = "eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|\\d".r
+
+    val firstDigit = regex.findFirstIn(str).map {
+      case "one" => 1
+      case "two" => 2
+      case "three" => 3
+      case "four" => 4
+      case "five" => 5
+      case "six" => 6
+      case "seven" => 7
+      case "eight" => 8
+      case "nine" => 9
+      case x => x.toInt
+    }.getOrElse(0)
+
+    val secondDigit = backwardsRegex.findFirstIn(str.reverse).map {
+      case "eno" => 1
+      case "owt" => 2
+      case "eerht" => 3
+      case "ruof" => 4
+      case "evif" => 5
+      case "xis" => 6
+      case "neves" => 7
+      case "thgie" => 8
+      case "enin" => 9
+      case x => x.toInt
+    }.getOrElse(0)
+
+    Seq(firstDigit, secondDigit)
+
   }
 
   def calculateCalibrationValue(input: String): Int = {
 
     val entries = input.split("\n")
 
-    entries.map(line => createNumberFromSet(getFirstAndLastDigitFromList(getDigitsFromLine(line)))).sum
+    entries.map(line => {
+      createNumberFromSet(getFirstAndLastDigit(line))
+    }).sum
 
   }
 
--- src/test/scala/aoc/day1/Day1Spec.scala
+++ src/test/scala/aoc/day1/Day1Spec.scala
@@ -8,32 +8,34 @@ class Day1Spec extends AnyWordSpec with Matchers {
 
   private val day1 = Day1()
 
-  "get digits from line" should {
-    "get all digits from line" in {
-      day1.getDigitsFromLine("1abc2") shouldBe Seq(1, 2)
-      day1.getDigitsFromLine("pqr3stu8vwx") shouldBe Seq(3, 8)
-      day1.getDigitsFromLine("a1b2c3d4e5f") shouldBe Seq(1,2,3,4,5)
-      day1.getDigitsFromLine("treb7uchet") shouldBe Seq(7)
-    }
-  }
-
   "getFirstAndLastDigit" should {
-
-    "return both digits if two" in {
-      day1.getFirstAndLastDigitFromList(Seq(1,2)) shouldBe Seq(1,2)
+    "get digits when numbers" in {
+      day1.getFirstAndLastDigit("1abc2") shouldBe Seq(1, 2)
+      day1.getFirstAndLastDigit("pqr3stu8vwx") shouldBe Seq(3, 8)
+      day1.getFirstAndLastDigit("a1b2c3d4e5f") shouldBe Seq(1, 5)
     }
 
-    "return first and last digits if three or more" in {
-      day1.getFirstAndLastDigitFromList(Seq(1, 2, 3)) shouldBe Seq(1, 3)
-      day1.getFirstAndLastDigitFromList(Seq(7, 3, 1, 54, 342)) shouldBe Seq(7, 342)
+    "repeat digit if only one" in {
+      day1.getFirstAndLastDigit("treb7uchet") shouldBe Seq(7, 7)
     }
 
-    "return same digits if just 1" in {
-      day1.getFirstAndLastDigitFromList(Seq(43)) shouldBe Seq(43, 43)
-    }
+    "find digits that are words" in {
+      day1.getFirstAndLastDigit("oneabc") shouldBe Seq(1, 1)
+      day1.getFirstAndLastDigit("two7sdfthreesdf") shouldBe Seq(2, 3)
+      day1.getFirstAndLastDigit("fourfivesixseveneightnine") shouldBe Seq(4, 9)
 
-    "return empty if no numbers" in {
-      day1.getFirstAndLastDigitFromList(Seq.empty) shouldBe Seq.empty
+      // Interesting part to parse here is the "twone" at the end
+      day1.getFirstAndLastDigit("honemkmbfbnlhtbq19twonekbp") shouldBe Seq(1, 1)
+
+      //Interesting part to parse here is the "eightwo" at the start
+      day1.getFirstAndLastDigit("eightwothree") shouldBe Seq(8, 3)
+
+      //Interesting part to parse here is the "eightwo" at the start
+      day1.getFirstAndLastDigit("eightwothree") shouldBe Seq(8, 3)
+
+      // Interesting part to parse here is the "twone" at the start
+      day1.getFirstAndLastDigit("xtwone3four") shouldBe Seq(2, 4)
+
     }
 
   }
@@ -56,6 +58,15 @@ class Day1Spec extends AnyWordSpec with Matchers {
       val input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
 
       day1.calculateCalibrationValue(input) shouldBe 142
+
+    }
+
+    "sum up the numeric values of each line with strings" in {
+
+      val input = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four" +
+        "\n4nineeightseven2\nzoneight234\n7pqrstsixteen"
+
+      day1.calculateCalibrationValue(input) shouldBe 281
 
     }
 
